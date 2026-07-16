737850ca5d8ee6a5814b7b7c8ac57cc72562e8b2
StasX <s.mestechkin@gmail.com>
2026-07-16 18:56:56 +0300

    Refactor cards

--- resources/js/ui/display.js
+++ resources/js/ui/display.js
@@ -3,57 +3,73 @@ const { removePlaylist } = require("../features/playlist/remove");
 const { updatePlaylist } = require("../features/playlist/update");
 const { AppStore } = require("../store/AppStore");
 
+function escapeHTML(str) {
+    return str.replace(/[&<>'"]/g, 
+        tag => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&#39;', '"': '&quot;' }[tag] || tag)
+    );
+}
 
 function displayPlaylist(playlist) {
-    const deg = playlist.name.length * Math.PI + 1;
-    $('#main-container').append(`
-    <div class="col-ls-3 col-md-4  col-sm-6 col-xs-12">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body" style="height: 400px">
-                <figure>
-                    <figcaption calss="len${playlist.name.length}">
-                        <div class="caption" style="transform: rotate(-${deg}deg)">${playlist.name}</div>
-                    </figcaption>
-                    <div class="container img-container">
-                        <div class="row">
-                            <div class=""col">
-                                <img width="200" height="200" alt="${playlist.name}" src="${playlist.image}">
+    const escapedName = escapeHTML(playlist.name);
+    const escapedImage = escapeHTML(playlist.image);
+    const card = $(`
+        <div class="col-lg-3 col-md-4 col-sm-6 col-12 card-container">
+            <div class="card quadratic-card">
+                <div class="card-body">
+                    <figure class="disk">
+                        <figcaption class="arc-text">${escapedName}</figcaption>
+                        <img class="disk-image" src="${escapedImage}" alt="${escapedName}">
+                        <div class="container btn-row">
+                            <div class="row">
+                                <div class="col">
+                                    <button class="edit">
+                                        <i class="fa-solid fa-pencil"></i>
+                                    </button>
+                                </div>
+                                <div class="col">
+                                    <button class="remove">
+                                        <i class="fa-solid fa-x"></i>
+                                    </button>
+                                </div>
+                                <div class="col">
+                                    <button class="play">
+                                        <i class="fa-solid fa-play"></i>
+                                    </button>
+                                </div>
                             </div>
-                        <div>
-                        <div class="row btn-row">
-                            <div class="col">
-                                <button class="edit">
-                                    <i class="fa-solid fa-pencil"></i>
-                                </button>
-                            </div>
-                            <div class="col">
-                                <button class="remove">
-                                    <i class="fa-solid fa-x"></i>
-                                </button>
-                            </div>
-                            <div class="col">
-                                <button class="play">
-                                    <i class="fa-solid fa-play"></i>
-                                </button>
-                            </div>
-                        <div>
-                    </div>
-                </figure>
+                        </div>
+                    </figure>
+                </div>
             </div>
         </div>
-    </div>
     `);
-    $('.card:last-child .caption').lettering();
-    $('.card:last-child .remove').on('click', function () { removePlaylist($(this)) });
-    $('.card:last-child .edit').on('click', function () { updatePlaylist($(this)) });
-    $('.card:last-child .play').on('click', () => {
 
+    $('#main-container>div').append(card);
+    const arcText = card.find(".arc-text");
+    arcText.lettering();
+    const letters = arcText.find("span");
+    const total = letters.length;
+    const startAngle = -65;
+    const endAngle = 65;
+    letters.each(function (index) {
+        const angle =
+            total === 1
+                ? 0
+                : startAngle +
+                  (endAngle - startAngle) * (index / (total - 1));
+
+        $(this).css("transform", `rotate(${angle}deg)`);
+    });
+
+    card.find('.remove').on('click', function () { removePlaylist($(this)); });
+    card.find('.edit').on('click', function () { updatePlaylist($(this)); });
+    card.find('.play').on('click', () => {        
     });
 }
 
 function displayPlaylists() {
     const store = AppStore.getInstance();
-    $('#main-container').html("");
+    $('#main-container>div').empty(); 
     $.each(store.getPlaylists(), (i, playlist) => {
         displayPlaylist(playlist);
     });
