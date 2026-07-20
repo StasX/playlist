32078124edd6a4e9d256102be1a8e7a1cf3f3961
StasX <s.mestechkin@gmail.com>
2026-07-20 19:13:05 +0300

    Refactor player

--- resources/js/features/player/modal.js
+++ resources/js/features/player/modal.js
@@ -3,7 +3,7 @@ const Swal = require("sweetalert2");
 const html = require("../../../views/player/player.html");
 
 function createModal(playlist) {
-    let currentSong;
+    let songName;
     Swal.fire({
         html: html,
         showConfirmButton: false,
@@ -14,20 +14,39 @@ function createModal(playlist) {
         allowEnterKey: true,
         didOpen: () => {
             const popup = $(Swal.getPopup());
+            popup.css({ "width": "800px", "border-radius": "300px" });
             const image = popup.find("img");
             image.attr("src", playlist.image);
+            const nowPlaying = popup.find("h5");
             const songsList = popup.find("ol");
-            currentSong=playlist.songs[0]?.name || "";
+            songName = playlist.songs[0]?.name || "";
             const audio = popup.find("audio");
-            
+
             $.each(playlist.songs, (i, song) => {
                 songsList.append(`<li>${song.name}</li>`)
                 audio.append(`<source src="${song.url}" type="audio/mpeg">`)
             });
-            const playing = audio.get(0);
-            $("title").text(`Playing: ${song.name}`)
-            playing.play();
 
+            let currentSong = 0;
+            const song = audio.get(currentSong);
+            const currentSongName = playlist.songs[currentSong].name
+            nowPlaying.text(`Now playing: ${currentSongName}`);
+            $("title").text(`Playing: ${currentSongName}`);
+            song.play();
+            const playPauseButton = popup.find("#play-pause");
+            audio.on("play", () => {
+                console.log("playing")
+                playPauseButton.html('<i class="fa-solid fa-pause"></i>');
+                image.addClass("rotation");
+            });
+            audio.on("pause", () => {
+                console.log("paused")
+                playPauseButton.html('<i class="fa-solid fa-play"></i>');
+                image.removeClass("rotation");
+            });
+            playPauseButton.click(() => {
+                song.paused ? song.play() : song.pause();
+            });
         },
     });
 }
